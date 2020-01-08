86340c1c686c8087b9dd570aa12cb91420c9ca5e
Andres Arias <andres@Andress-MBP.stny.rr.com>
2020-01-08 00:03:04 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,21 @@
 const cards = ["queen", "queen", "king", "king"];
-const cardsInPlay = [];
+let cardsInPlay = [];
 
-const cardOne = cards[0];
-const cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again");
-	}
+	};
 };
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo); 
+function flipCard (cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	};
+};
+
+flipCard(0);
+flipCard(1);
