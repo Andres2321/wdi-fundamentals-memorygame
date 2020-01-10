63a7c48d46f0fb26a479238bbd53684dc0e88336
Andres Arias <andres@Andress-MBP.stny.rr.com>
2020-01-10 10:39:29 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,29 @@
-const cards = ["queen", "queen", "king", "king"];
+
+
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
+
+
 let cardsInPlay = [];
 
 function checkForMatch() {
@@ -10,12 +35,17 @@ function checkForMatch() {
 };
 
 function flipCard (cardId) {
-	cardsInPlay.push(cards[cardId]);
-	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	};
 };
 
 flipCard(0);
-flipCard(1);
+flipCard(2);
+
+
+
