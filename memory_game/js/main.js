93044ec10597df54a0d47cee6767f8754b95e4bf
Andres Arias <andres@Andress-MacBook-Pro.local>
2020-01-12 12:33:40 -0500

    ceated game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,4 @@
-
-
-var cards = [
+const cards = [
 	{
 		rank: "queen",
 		suit: "hearts",
@@ -23,29 +21,54 @@ var cards = [
 	}
 ];
 
-
+// This variable will collect all cards clicked by the user 
 let cardsInPlay = [];
 
-function checkForMatch() {
+ 
+var checkForMatch = function(){
+	// Logic: If the rank of the first and second index in cardsInPlay equal
 	if (cardsInPlay[0] === cardsInPlay[1]) {
+		// If true display this alert
 		alert("You found a match!");
 	} else {
+		// If false display this alert
 		alert("Sorry, try again");
 	};
 };
 
-function flipCard (cardId) {
+
+var flipCard = function(){
+	// Attaches the data attribute to a variable called cardId
+	var cardId = event.target.getAttribute('data-id');
+	// Adds the rank contained in the cards array when it is "clicked" by the user
 	cardsInPlay.push(cards[cardId].rank);
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+	//  Attaches the source of the image contained in the cards array
+	event.target.setAttribute('src', cards[cardId].cardImage);
+	//  When element cardsInPlay has a total of two cards, then checkForMatch function will be called
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	};
 };
 
-flipCard(0);
-flipCard(2);
 
+var createBoard = function (){
+	// Iterates through the cards array
+	for (let i = 0; i <cards.length; i++){
+		// Creates an img element saved to a variable called cardElement
+		var cardElement = document.createElement('img');
+		// Attaches the source of an image to cardElement (back of a playing card)
+		cardElement.setAttribute('src', 'images/back.png');
+		// Creates data attribute as function iterates through cards array
+		cardElement.setAttribute('data-id', i);
+		// Creates an event handler and executes the callback function flipCard
+		document.addEventListener('click', flipCard);
+		// Attaches cardElement to the id 'game-board' in the index.html file
+		document.getElementById('game-board').appendChild(cardElement);
+	};
+};
+
+
+/* */
+createBoard();
 
 
